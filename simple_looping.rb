a10fb360c00cb39b01c89dcc77ac9ec40252ca2e
Bryna Dunnells <badunnells@gmail.com>
2019-05-02 16:37:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -46,7 +46,7 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
     
-    for int (1..8)
+    for 1..number_of_times
       puts phrase
 
     end
